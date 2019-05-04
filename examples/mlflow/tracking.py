ce2a938a43724a0e4272df6ea1aac24943292f2a
rallesiardo <robin.allesiardo@gmail.com>
2019-05-04 19:04:09 +0200

    Removing context manager

--- examples/mlflow/tracking.py
+++ examples/mlflow/tracking.py
@@ -3,25 +3,13 @@ from __future__ import print_function
 import mlflow.sklearn
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from contextlib import contextmanager
 
 if __name__ == "__main__":
 
     # Use an existing experiment or create a new one
     mlflow.set_experiment("tracking_experiment")
 
-    # A context manager to check if the run completed without error
-    @contextmanager
-    def check_completion(run):
-        try:
-            mlflow.log_param("State", "RUNNING")
-            yield run
-            mlflow.log_param("State", "COMPLETED")
-        except:
-            mlflow.log_param("State", "CRASHED")
-
-
-    with check_completion(mlflow.start_run()) as active_run:
+    with mlflow.start_run() as active_run:
 
         X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
         y = np.array([0, 0, 1, 1, 1, 0])
